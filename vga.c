634f9352e940cbb7ccc7858d137d46315e1f300a
pio387 <pio387@gmail.com>
2013-10-26 16:11:06 +0200

    Update vga.c
    Added info about monitor

--- vga.c
+++ vga.c
@@ -13,8 +13,10 @@ void vga_init()
 
 	if ((*((volatile u16int*) 0x410) & 0x30) == 0x30) // detecting monochrome monitor
 		vga_mem = (u16int*) 0xB0000;
+		vga_puts("Using monochrome monitor")
 	else
 		vga_mem = (u16int*) 0xB8000; // it's color
+		vga_puts("Using color monitor")
 }
 
 void vga_cls()
