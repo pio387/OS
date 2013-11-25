5f8034d53f665bcbef3d8a64780d08ea52ab557a
pio387 <pio387@gmail.com>
2013-11-25 18:53:57 +0100

    Irq.h done
    Irq handlers added!

--- irq.h
+++ irq.h
@@ -14,5 +14,36 @@ struct idt_pointer
         u32int base;
 }__attribute((packed))
 
+/* Here we have handlers to Interrupt Service Routines */
 
-extern void int_1;
+extern void int_1();
+extern void int_2();
+extern void int_3();
+extern void int_4();
+extern void int_5();
+extern void int_6();
+extern void int_7();
+extern void int_8();
+extern void int_9();
+extern void int_10();
+extern void int_11();
+extern void int_12();
+extern void int_13();
+extern void int_14();
+extern void int_15();
+extern void int_16();
+extern void int_17();
+extern void int_18();
+extern void int_19();
+extern void int_20();
+extern void int_21();
+extern void int_22();
+extern void int_23();
+extern void int_24();
+extern void int_25();
+extern void int_26();
+extern void int_27();
+extern void int_28();
+extern void int_29();
+extern void int_30();
+extern void int_31();
